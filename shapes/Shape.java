206a8275fd23c027b41a95e87814ec86840933ad
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2024-11-21 22:35:23 -0330

    shape package and Shape.java

---
+++ shapes/Shape.java
@@ -0,0 +1,21 @@
+package shapes;
+
+public abstract class Shape {
+    protected String name;
+
+    // Constructor
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    // Abstract methods for perimeter and area
+    public abstract double computePerimeter();
+
+    public abstract double computeArea();
+
+    // toString method for displaying the object information
+    @Override
+    public String toString() {
+        return "Shape: " + name + ", Area: " + computeArea() + ", Perimeter: " + computePerimeter();
+    }
+}
