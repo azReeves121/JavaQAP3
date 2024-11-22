3234e105a6305b84f5f941764d17001b7b1f15e6
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2024-11-22 00:01:43 -0330

    fix up

--- ShapeInterface/Demo.java
+++ ShapeInterface/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
         Shape[] shapes = new Shape[4];
         shapes[0] = new Circle(7.0);
         shapes[1] = new Ellipse(5.0, 9.0);
-        shapes[2] = new Triangle(2.0, 4.0, 6.0);
+        shapes[2] = new Triangle(8.0, 4.0, 6.0);
         shapes[3] = new EquilateralTriangle(6.0);
 
         // Print shapes before scaling
