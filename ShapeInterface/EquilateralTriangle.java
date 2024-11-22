6e3438695cf26d0cfa1189abbe48bd9e5b0b093f
azReeves121 <147558144+azReeves121@users.noreply.github.com>
2024-11-21 23:42:23 -0330

    Interface EquilateralTriangle

---
+++ ShapeInterface/EquilateralTriangle.java
@@ -0,0 +1,9 @@
+package ShapeInterface;
+
+public class EquilateralTriangle extends Triangle {
+    // Constructor
+    public EquilateralTriangle(double side) {
+        super(side, side, side);
+        this.name = "Equilateral Triangle"; // Override name
+    }
+}
